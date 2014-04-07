6b8a0dd4fa65a83ebf4d1ee94ac3fdb2865743d6
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2014-04-07 18:24:56 +0400

    Library name is no longer hard-coded.

--- test-callout-dlopen.c
+++ test-callout-dlopen.c
@@ -7,9 +7,22 @@
 
 #include <stdio.h>
 #include <dlfcn.h>
+#include <libgen.h>
 
-int main (const int argc, const char *argv[]) {
-	void *handle = dlopen("/Users/ashcheglov/docs/programming/c/test-callout/libtest-callout.dylib", RTLD_NOW);
+int main (const int argc, char *argv[]) {
+	const char *executable_path = dirname(argv[0]);
+	char library_path[1024];
+#ifdef _WIN32
+	sprintf(library_path, "%s\\test-callout.dll", executable_path);
+#else // _WIN32
+#ifdef __APPLE__
+	sprintf(library_path, "%s/libtest-callout.dylib", executable_path);
+#else // __APPLE__
+	sprintf(library_path, "%s/libtest-callout.so", executable_path);
+#endif // __APPLE__
+#endif // _WIN32
+
+	void *handle = dlopen(library_path, RTLD_NOW);
 	if (!handle) {
 		printf("%s\n", dlerror());
 		return 1;
